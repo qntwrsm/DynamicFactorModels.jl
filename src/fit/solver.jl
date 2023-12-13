d143da1e67dc4e7d73b3f5a1ae25a18bb79d6748
Quint Wiersma <qnt.wrsm@gmail.com>
2023-12-13 12:01:10 +0100

    Add Kalman filter and smoother routines

--- src/fit/solver.jl
+++ src/fit/solver.jl
@@ -8,3 +8,29 @@ solver.jl
 
 @date: 2023/12/08
 =#
+
+"""
+    update!(model)
+
+Update parameters of dynamic factor model `model` using an
+expectation-maximization (EM) solve w/ regularization term given by
+`regularizer`.
+"""
+function update!(model::DynamicFactorModel, regularizer::NamedTuple)
+    # (E)xpectation step
+    (α̂, V, Γ) = smoother(model)
+    factors(model) .= α̂
+    
+    # (M)aximization step
+    # update factor loadings and dynamics
+    update_loadings!(loadings(model), data(model), factors(model), V, regularizer.factors)
+    update_transition!(process(model), V, Γ)
+
+    # update mean specification
+    update!(mean(model), regularizer.mean)
+
+    # update error specification
+    update!(errors(model), regularizer.error)
+
+    return nothing
+end
--- src/utilities.jl
+++ src/utilities.jl
@@ -30,7 +30,7 @@ function simulate(F::FactorProcess; burn::Integer=100, rng::AbstractRNG=Xoshiro(
         if t == 1
             ft .= f_prev
         else
-            ft .= dynamics(F) * f_sim[:,t-1] + rand(rng, dist(F))
+            ft .= dynamics(F) * f_sim[:,t-1] + randn(rng, size(F))
         end
     end
 
@@ -59,4 +59,131 @@ function simulate(ε::SpatialMovingAverage; rng::AbstractRNG=Xoshiro())
     end
 
     return SpatialMovingAverage(e_sim, MvNormal(Diagonal(var(ε))), copy(spatial(ε)), weights(ε))
+end
+
+"""
+    state_space(model) -> (y_star, Z_star, d_star, a1, P1)
+
+State space form of the collapsed dynamic factor model `model`.
+"""
+function state_space(model::DynamicFactorModel)
+    R = size(process(model))
+    Ty = eltype(data(model))
+
+    # projection components
+    if errors(model) isa Simple
+        Zt_Hinv = loadings(model)' / cov(errors(model)) 
+    elseif errors(model) isa SpatialAutoregression
+        Hinv = poly(errors(model))' * cov(errors(model)) \ poly(errors(model)) 
+        Zt_Hinv = loadings(model)' * Hinv
+    elseif errors(model) isa SpatialMovingAverage
+        H = poly(errors(model)) * cov(errors(model)) * poly(errors(model))'
+        Zt_Hinv = loadings(model)' / H
+    end
+    Zt_Hinv_Z = Zt_Hinv * loadings(model)
+    
+    # Cholesky decomposition
+    C = cholesky(Hermitian(inv(Zt_Hinv_Z)))
+
+    # collapsing projection matrix
+    A_star = C.U * Zt_Hinv
+
+    # collapsing
+    y_star = [A_star * yt for yt ∈ eachcol(data(model))]
+    Z_star = inv(C.L)
+    if mean(model) isa ZeroMean
+        d_star = [Zeros(Ty, R) for _ ∈ axes(data(model), 2)]
+    elseif mean(model) isa Exogenous
+        d_star = [A_star * μt for μt ∈ eachcol(mean(mean(model)))]
+    end
+
+    # initial conditions
+    a1 = zeros(Ty, R)
+    P1 = Matrix{Ty}(I, R, R)
+
+    return (y_star, Z_star, d_star, a1, P1)
+end
+
+"""
+    filter(model) -> (a, P, v, F, K)
+
+Collapsed Kalman filter for the dynamic factor model `model`. Returns the
+filtered state `a`, covariance `P`, forecast error `v`, forecast error variance
+`F`, and Kalman gain `K`.
+"""
+function filter(model::DynamicFactorModel)
+    # collapsed state space system
+    (y, Z, d, a1, P1) = state_space(model)
+    T = dynamics(model)
+
+    # initialize filter output
+    a = similar(y, typeof(a1))
+    P = similar(y, typeof(P1))
+    v = similar(y)
+    F = similar(y, typeof(P1))
+    K = similar(y, typeof(P1))
+
+    # initialize filter
+    a[1] = a1
+    P[1] = P1
+
+    # filter
+    for t ∈ eachindex(y)
+        # forecast error
+        v[t] = y[t] - Z * a[t] - d[t]
+        F[t] = Z * P[t] * Z' + I
+
+        # Kalman gain
+        K[t] = T * P[t] * Z' / F[t]
+
+        # prediction
+        if t < length(y)
+            a[t+1] = T * a[t] + K[t] * v[t]
+            P[t+1] = T * P[t] * (T - K[t] * Z)' + I
+        end
+    end
+
+    return (a, P, v, F, K)
+end
+
+"""
+    smoother(model) -> (α̂, V, Γ)
+
+Collapsed Kalman smoother for the dynamic factor model `model`. Returns the
+smoothed state `α̂`, covariance `V`, and autocovariance `Γ`.
+"""
+function smoother(model::DynamicFactorModel)
+    # collapsed state space system
+    (y, Z, _, a1, P1) = state_space(model)
+    T = dynamics(model)
+
+    # filter
+    (a, P, v, F, K) = filter(model)
+
+    # initialize smoother output
+    α̂ = similar(a)
+    V = similar(P)
+    Γ = similar(P, length(y) - 1)
+
+    # initialize smoother
+    r = zero(a1)
+    N = zero(P1)
+    L = similar(P1)
+
+    # smoother
+    for t ∈ reverse(eachindex(y))
+        L .= T - K[t] * Z
+
+        # backward recursion
+        r .= Z' / F[t] * v[t] + L' * r
+        N .= Z' / F[t] * Z + L' * N * L
+
+        # smoothing
+        α̂[t] = a[t] + P[t] * r
+        V[t] = P[t] - P[t] * N * P[t]
+        t > 1 && (Γ[t-1] = I - P[t] * N)
+        t < length(y) && (Γ[t] *= L * P[t])
+    end
+
+    return (α̂, V, Γ)
 end
